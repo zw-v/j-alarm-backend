bf2215a3e384d8658c3b7e17b26175f278447bc2
Zbigniew Wojciechowski <zwojciechowski@one2tribe.pl>
2022-09-19 20:51:53 +0200

    empty

---
+++ src/main/java/com/dayport/jalarmbackend/JAlarmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dayport.jalarmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JAlarmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JAlarmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dayport/jalarmbackend/JAlarmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayport.jalarmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JAlarmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
